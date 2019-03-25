44450035e135fda41843904df7c017e3ab627530
Kohinoor Gupta <kohinoor.gupta@gmail.com>
2019-03-25 14:48:36 +0530

    interview bullshit

--- others/interview/src/main/java/interview/threading/Deadlock101.java
+++ others/interview/src/main/java/interview/threading/Deadlock101.java
@@ -2,6 +2,8 @@ package interview.threading;
 
 import java.util.concurrent.CountDownLatch;
 
+import static java.lang.Thread.currentThread;
+
 public final class Deadlock101 {
   private static final CountDownLatch latch = new CountDownLatch(2);
 
@@ -9,8 +11,8 @@ public final class Deadlock101 {
     Lock one = new Lock("one");
     Lock two = new Lock("two");
 
-    Thread a = new Thread(new LockConsumer(one, two, "A"));
-    Thread b = new Thread(new LockConsumer(two, one, "B"));
+    Thread a = new Thread(new LockConsumer(one, two), "A");
+    Thread b = new Thread(new LockConsumer(two, one), "B");
     a.start();
     b.start();
 
@@ -23,23 +25,23 @@ public final class Deadlock101 {
     private Lock(String name) {this.name = name;}
   }
 
-  private static final class LockConsumer implements Runnable{
+  private static final class LockConsumer implements Runnable {
     private final Lock a, b;
-    private final String name;
 
-    private LockConsumer(Lock a, Lock b, String name) {
+    private LockConsumer(Lock a, Lock b) {
       this.a = a;
       this.b = b;
-      this.name = name;
     }
 
     public void run() {
-      synchronized (a) {
-        System.out.println(name + " -- [acquired lock] --> " + a.name);
-        synchronized (b) {
-          latch.countDown();
-          System.out.println(name + " -- [acquired lock] --> " + b.name);
-          System.out.println("No deadlock yet ...");
+      while (true) {    // because of timing issues one thread can acquire both locks and then the other thread is `run`
+        synchronized (a) {
+          System.out.println(currentThread().getName() + " -- [acquired lock] --> " + a.name);
+          synchronized (b) {
+            latch.countDown();
+            System.out.println(currentThread().getName() + " -- [acquired lock] --> " + b.name);
+            System.out.println("No deadlock yet ...");
+          }
         }
       }
     }
